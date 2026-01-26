6a388ac99188e02fc1ac3c7b275be913882ab167
Krishna Addepalli <coolkrishna31@gmail.com>
2026-01-26 19:32:58 +0100

    Fix stack package issues for vocab1.hs

--- src/vocab1.hs
+++ src/vocab1.hs
@@ -1,9 +1,10 @@
 import Data.Char
 import Data.List (group, sort)
-import Data.Text qualified as T
-import Data.Text.IO qualified as TIO
+import qualified Data.Text  as T
+import qualified Data.Text.IO as TIO
 import System.Environment -- Module for reading commandline arguments into a list of strings
 
+main :: IO ()
 main = do
   [fname] <- getArgs
   text <- TIO.readFile fname
